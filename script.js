8134fe16fec525e5319eb82d673c9a4387ef0874
Arthur Gervasio <sismanarthur@gmail.com>
2026-02-07 21:42:47 -0300

    exibição da soma dos números sorteados adicionada

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let resultados = [];
+let soma = 0;
 let faces = document.getElementById("faces");
 let quantidade = document.getElementById("quantity");
 
@@ -9,15 +10,20 @@ function sortear() {
     resultados = [];
     for(let i = 0; i < quantidade.value; i++) {
         resultados.push(parseInt(Math.random()*parseInt(faces.value)+1));
+        soma += resultados[i];
     }
-    document.querySelector(".dice-area").textContent = resultados;
+    document.getElementById("results").textContent = resultados;
+    document.getElementById("totalSum").textContent = soma;
     faces.value = "";
     quantidade.value = "";
+    soma = 0;
 }
 
 function resetar() {
     resultados = [];
     faces.value = "";
     quantidade.value = "";
-    document.querySelector(".dice-area").textContent = "";
+    soma = 0;
+    document.getElementById("results").textContent = "";
+    document.getElementById("totalSum").textContent = "0";
 }
