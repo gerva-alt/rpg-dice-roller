7dd652545a70c89ac8bc19a1df87bc546369bf30
Arthur Gervasio <sismanarthur@gmail.com>
2026-02-07 21:34:48 -0300

    verificação de números nos inputs adicionada

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let faces = document.getElementById("faces");
 let quantidade = document.getElementById("quantity");
 
 function sortear() {
+    if(isNaN(faces.value || quantidade.value) || faces.value < 1 || quantidade.value < 1){
+        return alert("Coloque números válidos!");
+    }
     resultados = [];
     for(let i = 0; i < quantidade.value; i++) {
         resultados.push(parseInt(Math.random()*parseInt(faces.value)+1));
