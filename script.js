e5aefcd7d9efff45f00793aef2c131ada902d484
Arthur Gervasio <sismanarthur@gmail.com>
2026-02-07 22:09:03 -0300

    mudança na forma como os números sorteados são implementados realizada com o intuito de impedir que os números fugissem do espaço designado

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let resultados = [];
+let textoResultados = document.getElementById("results");
 let soma = 0;
+let textoSoma = document.getElementById("totalSum");
 let faces = document.getElementById("faces");
 let quantidade = document.getElementById("quantity");
 
 function sortear() {
+    textoResultados.innerHTML = "";
     if(isNaN(faces.value || quantidade.value) || faces.value < 1 || quantidade.value < 1){
-        return alert("Coloque números válidos!");
+        return alert("Insira valores válidos!");
     }
     if(faces.value > 100){
-        return alert("Você não deve usar um dado de mais de 100 faces!");
+        return alert("Você não deve usar um dado com mais que 100 faces!");
     }
     if(quantidade.value > 100){
         return alert("Você não deve rolar mais que 100 dados!")
@@ -17,19 +20,21 @@ function sortear() {
     for(let i = 0; i < quantidade.value; i++) {
         resultados.push(parseInt(Math.random()*parseInt(faces.value)+1));
         soma += resultados[i];
+        var span = document.createElement("span");
+        span.textContent = resultados[i];
+        textoResultados.appendChild(span);
     }
-    document.getElementById("results").textContent = resultados;
-    document.getElementById("totalSum").textContent = soma;
+    textoSoma.textContent = soma;
     faces.value = "";
     quantidade.value = "";
     soma = 0;
 }
 
 function resetar() {
-    resultados = [];
     faces.value = "";
     quantidade.value = "";
+    resultados = [];
+    textoResultados.textContent = "";
     soma = 0;
-    document.getElementById("results").textContent = "";
-    document.getElementById("totalSum").textContent = "0";
+    textoSoma.textContent = "0";
 }
